9a9a378cb5927342bee500879cacad5daae51617
Talefe <saratftalefe@gmail.com>
2017-11-23 19:47:12 +0000

    minor changes

--- src/main/java/org/academiadecodigo/hackathon/jesusfindr/controller/InitalScreenController.java
+++ src/main/java/org/academiadecodigo/hackathon/jesusfindr/controller/InitalScreenController.java
@@ -1,6 +1,21 @@
 package org.academiadecodigo.hackathon.jesusfindr.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import org.academiadecodigo.hackathon.jesusfindr.Navigation;
+
 public class InitalScreenController implements Controller {
 
+    @FXML
+    private Pane welcomeView;
 
+    @FXML
+    private Button loginButton;
+
+    public void onLogin() {
+
+        Navigation.getInstance().loadScreen("loginscreen");
+    }
 }
+
